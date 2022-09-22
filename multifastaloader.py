53ebe1e362ab50ae137f9e4b7c570fa70d3eb29e
Megabyte <NyabutiM@protonmail.com>
2022-09-22 06:07:28 +0300

    Update multifastaloader.py
    Edit multifastloader.py:
    
    Thanks so much for the function @Wangari-Joyce. There are some tweaks I have added mostly pythonic conventions to prevent issues in the long run:
    
    * Formatted with [black(22.3.0 on py 3.7) package](https://black.readthedocs.io/en/stable/the_black_code_style/index.html). Command run `black multifastaloader.py`.
    * Added description about the os package and provided an example of method getenv.
    * Added an * to the multifastaloader since it is variable argument. Check out *args and *kwargs documentation.
    * Linter (Pylint 2.13.9 on py 3.7) threw error that Bio is not used in the code. Code rating moved from 6.17/10 -> 7.50/10.  
    * Linter threw error that the encoding hadn't been specified with open function. Added fix. Command run `pylint multifastaloader.py. Better `pylint --disable=R,C --errors-only multifastaloader.py`.
    * Minor edits to the py file.

--- multifastaloader.py
+++ multifastaloader.py
@@ -1,32 +1,49 @@
-#Importing the libraries and modules that we are going to use  to retrieve the data from NCBI
-#Required python library: Biopython
-# Required modules: 
-# 1.Entrez for retrieving sequences 
-# SeqIO for exploring the sequences
+# Importing the libraries and modules that we are going to use  to retrieve the data from NCBI
+# Required python library: Biopython
+# Required modules:
+# 1.Entrez for retrieving sequences
+# 2.SeqIO for exploring the sequences
+# 3.os provides functions for interacting with the operating system
 
 import os
-import Bio
 from Bio import Entrez
 from Bio import SeqIO
 
 
-#To use NCBI'S E-Utilities you must specify your email address with each request
-Entrez.email= os.getenv('email')
+# To use NCBI'S E-Utilities you must specify your email address with each request
+# This function grabs your email from your .bashrc file
+# it should look like this to work export email="johndoe@provider.com"
+Entrez.email = os.getenv("email")
 
-def multifastaloader(accession_numbers):
-    '''This function takes a list of accession numbers as input,
-    fetches the fasta sequences from NCBI and then outputs them in a multi fasta file ;sequences.fasta'''
-    
+
+def multifastaloader(*accession_numbers):
+    """This function takes a list of accession numbers as input,
+    fetches the fasta sequences from NCBI and then outputs them in a multi fasta file ;sequences.fasta
+
+    Parameters
+    ----------
+    *accession_numbers : a list of strings with accession numbers used in NCBI to retrieve sequences with those identifiers.
+
+
+    Returns
+    -------
+    type
+        Nonetype
+    A single multifasta file is returned to the working directory.
+
+    """
+
     for i in accession_numbers:
-        query=Entrez.efetch(db='nucleotide',id=i, rettype='fasta', retmode='text')
-        record=SeqIO.parse(query,'fasta')   #converting the query to a sequence record in biopython inorder to explore the sequences
+        query = Entrez.efetch(db="nucleotide", id=i, rettype="fasta", retmode="text")
+        record = SeqIO.parse(
+            query, "fasta"
+        )  # converting the query to a sequence record in biopython inorder to explore the sequences
 
-# writing the query to a file
-        with open ('sequences.fasta' ,'a') as multi_fasta:
-            SeqIO.write(record, multi_fasta, 'fasta')
-            
-            
-            
+        # writing the query to a file
+        with open("sequences.fasta", "a", encoding="UTF-8") as multi_fasta:
+            SeqIO.write(record, multi_fasta, "fasta")
+
+
 accession_no = [
     "LN871587.1",
     "FM207520.1",
@@ -49,7 +66,6 @@ accession_no = [
     "LT599799.2",
     "KT378441.1",
 ]
-
 
 
 multifastaloader(accession_no)
