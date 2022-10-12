3b258b8240e5efe977990926a13079b64cf3e7c1
Megabyte <NyabutiM@protonmail.com>
2022-10-12 12:19:15 +0300

    Update load_multifasta.py
    Edit load_multifasta.py:
         - Add the sequences of gc, ga and length

--- load_multifasta.py
+++ load_multifasta.py
@@ -1,13 +1,32 @@
 # IPython log file
 
 get_ipython().run_line_magic('logstart', 'load_multifasta.py')
+
+from Bio import SeqIO
+from Bio.SeqUtils import GC
+
+def GA(seq):
+    ga = sum(seq.count(x) for x in ["G", "A", "g", "a", "S", "s"])
+    try:
+        return ga * 100.0 / len(seq)
+    except ZeroDivisionError:
+        return 0.0
+
+
 sequences = []
 identifiers = []
-from Bio import SeqIO
+gc_content = []
+ga_content = [] 
+length_seq= []
+
 
 with open("sequences.fasta") as handle:
     for record in SeqIO.parse(handle, "fasta"):
+        gc_content.append(GC(str(record)))
+        ga_content.append(GA(str(record)))
         identifiers.append(record.id)
         sequences.append(record.seq)
+        length_seq.append(len(record))
+        dataframe = pd.DataFrame(zip(identifiers, length_seq, gc_content, ga_content), columns=["accession_id", "seq_length", "gc_content", "ga_content"])
         
 exit()
